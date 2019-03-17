c41a2867914fde8e2bebfbd5696d183dd9c254f3
viktimolog <viktimolog@gmail.com>
2019-03-17 16:45:24 +0200

    Added form and addTodo function.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { TodoComponent } from './components/todo/todo.component';
     TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Todo } from "../../models/Todo";
 
 
@@ -9,9 +9,20 @@ import { Todo } from "../../models/Todo";
 })
 export class TodoComponent implements OnInit {
   todos: Todo[];
+
+  todo: Todo = {
+      id: null,
+      title: '',
+      text: '',
+      complete: false
+  };
+
+  @ViewChild('form') form;
+
   constructor() { }
 
   ngOnInit() {
+    //Get todos from DB
     this.todos = [
         {
           id: 1,
@@ -20,15 +31,42 @@ export class TodoComponent implements OnInit {
           complete: false
         },
         {
-            id: 2,
-            title: 'Task 2',
-            text: 'My task 2',
-            complete: true
+          id: 2,
+          title: 'Task 2',
+          text: 'My task 2',
+          complete: true
         }
     ];
   }
+
   //Delete task
-    deleteTask(id): void{
-        this.todos = this.todos.filter(todo => todo.id !== id);
-    }
+  deleteTask(id): void{
+      this.todos = this.todos.filter(todo => todo.id !== id);
+  }
+
+  //Trackby
+  identify = index => index;
+
+  addTodo = () => {
+      const { title, text } = this.form.value;
+
+      // values from [(ngModel)]
+      const newTodo = {
+          id: this.todos.length + 1,
+          title: this.todo.title,
+          text: this.todo.text,
+          complete: false
+      };
+
+      // values from form
+      const newTodo1 = {
+          id: this.todos.length + 1,
+          title,
+          text,
+          complete: false
+      };
+
+      this.todos.unshift(newTodo1);
+      this.form.reset();
+  }
 }
